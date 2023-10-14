0cbc83e7fd67102726faea19269974af54a7fff5
Nicolas Sanatana Kruger <nicolassantanakruger@gmail.com>
2023-10-14 10:51:04 -0300

    update: type WeatherIconsResponse

--- src/services/getWeatherByCity.ts
+++ src/services/getWeatherByCity.ts
@@ -9,6 +9,8 @@ interface GetWeatherByCityProps {
   longitude: number;
 }
 
+type WeatherIconsResponse = "Clouds" | "Rain" | "Clear" | "Snow";
+
 export interface WeatherAPIResponseProps {
   list: {
     pop: number;
@@ -27,7 +29,7 @@ export interface WeatherAPIResponseProps {
       speed: number;
     };
     weather: {
-      main: "Clouds" | "Rain" | "Clear" | "Snow";
+      main: WeatherIconsResponse;
       description: string;
     }[];
   }[];
